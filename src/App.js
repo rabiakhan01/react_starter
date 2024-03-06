cd3d4f012ac40083d06e2e768fd38ee25b4e3823
rabiakhan01 <rabiakham789@gmail.com>
2024-03-06 16:02:19 +0500

    Tailwind is added in project

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Design your app using tailwind 
         </a>
       </header>
     </div>
