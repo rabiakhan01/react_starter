07877353d8a0330549405c3226492b11f237b273
rabiakhan01 <rabiakham789@gmail.com>
2024-03-08 17:46:38 +0500

    A new branch portfolio is created

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import '../src/assets/fonts/fonts.css';
 import imageSource from '../src/assets/images/images';
-import 'rc-slider/assets/index.css';
-import Slider from 'rc-slider';
-import color, { blackColor, blue, greenColor, greenTheme, oliveColor, theme, whiteColor } from './utils/colors';
 function App() {
   return (
     <div className="App">
